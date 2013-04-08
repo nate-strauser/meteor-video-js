ff4c19f2a804be62c4e93fb42eef320368d3c6fe
Nathan Strauser <nathan.strauser@gmail.com>
2013-04-08 17:51:52 -0400

    tweak load

--- video-js-load.js
+++ video-js-load.js
@@ -1,10 +1,11 @@
 //Functions to run after the script tag has loaded
 var videojsLoadCallback = function(){
+	_V_.options.flash.swf = "https://vjs.zencdn.net/c/video-js.swf";
 };
 
 //If the script doesn't load
 var videojsErrorCallback = function(error){
-    if(typeof console != undefined) {
+    if(typeof console !== undefined) {
         console.log(error);
     }
 };
@@ -15,14 +16,14 @@ var head = document.getElementsByTagName('head')[0];
 var style = document.createElement('link');
 style.type = 'text/css';
 style.rel = "stylesheet";
-style.src = '//vjs.zencdn.net/c/video-js.css';
+style.href = 'https://vjs.zencdn.net/c/video-js.css';
 
 head.appendChild(style);
 
 //Generate a script tag
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = '//vjs.zencdn.net/c/video.js';
+script.src = 'https://vjs.zencdn.net/c/video.js';
 script.onload = videojsLoadCallback;
 script.onerror = videojsErrorCallback;
 
