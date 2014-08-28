7bc5a82da41ddd15c485cd08bf0fd1d147c6cf41
Nathan Strauser <nathan.strauser@gmail.com>
2014-08-27 21:03:30 -0400

    0.9.0 update

--- package.js
+++ package.js
@@ -1,7 +1,11 @@
 Package.describe({
-      summary: "A smart package that makes it easy to use Video.js"
+      name:"natestrauser:videojs",
+      summary: "A smart package that makes it easy to use Video.js",
+  version: "4.1.3",
+  git: "https://github.com/nate-strauser/meteor-video-js.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
     api.add_files('video-js-load.js', 'client');
 });
